83777a04e38cf27de7fff114857813a3eb8373c6
tvk2-user <tvk2@alu.ua.es>
2026-02-21 23:08:45 +0100

    Generos actualizado

---
+++ clases/generos.py
@@ -0,0 +1,13 @@
+class Genero:
+    def __init__(self, nombre, descripcion):
+        self.nombre = nombre
+        self.descripcion = descripcion
+
+    def __str__(self):
+        return (f"Género: {self.nombre}\n"
+                f"Descripción: {self.descripcion}")
+
+    def __repr__(self):
+        return (f"{type(self).__name__}("
+                f"nombre='{self.nombre}', "
+                f"descripcion='{self.descripcion}')")
