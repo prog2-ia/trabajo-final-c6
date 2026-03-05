213c9516065b4a5eb7492ea71d566cc0b98e6dda
tvk2-user <tvk2@alu.ua.es>
2026-03-05 18:16:04 +0100

    Actualización del estado de ánimo
    Métodos nuevos:
    -Positivo
    -Negativo(Para posteriormente crear playlist)
    -Cambiar estado de animo

--- clases/estados_de_animo.py
+++ clases/estados_de_animo.py
@@ -11,3 +11,19 @@ class EstadoAnimo:
         return (f"{type(self).__name__}("
                 f"nombre='{self.nombre}', "
                 f"descripcion='{self.descripcion}')")
+
+    # Métodos Estado_animo
+
+    # Determinar si es un estado de animo positivo: Necesario para crear playlist
+    def es_positivo(self):
+        positivos = ["Feliz", "Entusiasmado", "Motivado", "Alegre"]
+        return self.nombre in positivos
+    #Estado de ánimo negativo
+    def es_negativo(self):
+        return not self.es_positivo()
+
+    # Cambia el estado de ánimo a uno nuevo.
+
+    def cambiar_estado(self, nuevo_nombre, nueva_descripcion):
+        self.nombre = nuevo_nombre
+        self.descripcion = nueva_descripcion
