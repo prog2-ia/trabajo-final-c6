6edf85730ebf9ff7d731f2255dc9fc1ffba7e756
tvk2-user <tvk2@alu.ua.es>
2026-03-05 18:19:16 +0100

    Add methods for genre popularity and updates
    Added methods to check genre popularity and update genre details.

--- clases/generos.py
+++ clases/generos.py
@@ -11,3 +11,13 @@ class Genero:
         return (f"{type(self).__name__}("
                 f"nombre='{self.nombre}', "
                 f"descripcion='{self.descripcion}')")
+
+    #Método para indicar la popularidad de un género: Importarte para crear Playlist de géneros concretos con canciones populares.
+     def es_popular(self):
+        populares = ["Pop", "Rock", "Reggaetón", "Hip Hop", "Electrónica"]
+        return self.nombre in populares
+
+    #Cambia el nombre y descripción del género.
+    def actualizar_genero(self, nuevo_nombre, nueva_descripcion):
+        self.nombre = nuevo_nombre
+        self.descripcion = nueva_descripcion
