ac7db49f5463e140d58d86bad5bf601a02c4eba3
piklowski <kmp8@alu.ua.es>
2026-03-06 13:06:31 +0100

    cambiar canciones2 por canciones

--- clases/canciones.py
+++ clases/canciones.py
@@ -1,48 +1,19 @@
-#Librería para formatear la fecha
-from datetime import datetime
+# clase alternativa para canciones (usando herencia)
 
-#Clase Canción
-class Cancion:
-    def __init__(self, titulo, artista, album, genero,duracion, fecha_lanzamiento, num_escuchas, discografica):
-        #Argumentos de instancia
-        self.titulo = titulo
-        self.artista = artista
-        self.album = album
-        self.duracion = int(duracion) #Tipo entero
-        self.num_escuchas = int(num_escuchas) #Tipo entero
-        self.discografica = discografica
+from contenido import Contenido
 
+class Cancion(Contenido):
+    def __init__(self,titulo, fecha_lanzamiento, duracion,genero,artista, discografia):
+        super().__init__(titulo,fecha_lanzamiento,duracion,genero,artista,estado_reproduccion=False)
+        self.discografia = discografia
 
-        # Lista de géneros en caso de que pertenezca a más de uno
-        if genero is None:
-            self.genero = []
-        elif isinstance(genero, list):
-            self.genero = genero
-        else:
-            self.genero = [genero]
+    def mostrar_info(self):
+        print('==== CANCION ====')
+        super().mostrar_info()
+        print(f'Discografia: {self.discografia}')
 
-        # Comprueba si es un string y cambia el formato
-        if isinstance(fecha_lanzamiento, str):
-            self.fecha_lanzamiento = datetime.strptime(fecha_lanzamiento, "%Y-%m-%d")
-        else:
-            self.fecha_lanzamiento = fecha_lanzamiento
+#-----------------------------------------
+if __name__ == '__main__':
+    cancion1 = Cancion("Livin' on a Prayer",1986,'04:11',['Glam metal','Hard rock'],'Bon Jovi', 'Slippery When Wet')
+    cancion1.mostrar_info()
 
-    def __str__(self):
-        return (f"{self.titulo} - {self.artista}\n"
-                f"Álbum: {self.album}\n"
-                f"Género(s): {', '.join(self.genero)}\n"
-                f"Duración: {self.duracion} seg\n"
-                f"Escuchas: {self.num_escuchas}\n"
-                f"Discográfica: {self.discografica}\n"
-                f"Fecha lanzamiento: {self.fecha_lanzamiento.strftime('%Y-%m-%d')}")
-
-    def __repr__(self):
-        return (f"{type(self).__name__}("
-                f"titulo='{self.titulo}', "
-                f"artista='{self.artista}', "
-                f"album='{self.album}', "
-                f"genero={self.genero}, "
-                f"duracion={self.duracion}, "
-                f"fecha_lanzamiento='{self.fecha_lanzamiento.strftime('%Y-%m-%d')}', "
-                f"num_escuchas={self.num_escuchas}, "
-                f"discografica='{self.discografica}')")
